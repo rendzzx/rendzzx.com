cec1551944c994010e55f1ac2718da868dfecd72
Rendy <lordkawe111@gmail.com>
2025-10-05 19:46:13 +0700

    next-contentlayer fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
-import { withContentlayer } from "next-contentlayer";
+// import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+  },
 };
 
-export default withContentlayer(nextConfig);
+// export default withContentlayer(nextConfig);
+
+// HAPUS wrapper withContentlayer
+export default nextConfig;
