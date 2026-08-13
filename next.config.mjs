f67117a1cd3242989da90a639465d3d959ee4195
Rendy Maulana <lordkawe1111@gmail.com>
2026-08-14 02:17:08 +0700

    build: upgrade Next.js to 16 and React to 19

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-// import { withContentlayer } from "next-contentlayer";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  experimental: {
-    mdxRs: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
   },
 };
 
-// export default withContentlayer(nextConfig);
-
-// HAPUS wrapper withContentlayer
 export default nextConfig;
